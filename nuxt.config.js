d653e6ce000f33fbf797c0bef033a7bfdff89be5
fathurzoy <fathurrahmanf123@gmail.com>
2021-12-18 11:59:58 +0700

    Mempersiapkan Integrasi API

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,7 +49,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: 'http://backer-backend.buildwithangga.id/',
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
